7f054b9051bfc7b0413f94eace59fa870193f52d
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-06 23:27:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string){
-  return string.toUpperCase
+  return string.toUpperCase;
 }
 console.log(function logShout())

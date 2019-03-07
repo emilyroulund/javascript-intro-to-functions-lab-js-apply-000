6a12ea53e3446083b549e338c0aff7cb542b30ac
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-07 00:09:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,7 @@ function sayHiToGrandma(string){
   if (string.toUpperCase()===string) 
   return "YES INDEED!";
 }
-function 
+function sayHiToGrandma(string){
+  if (string = "I love you, Grandma.")
+  return "I love you, too."
+}

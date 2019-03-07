1295cb2ca7438dd23df797c769726e52a543641e
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-07 00:13:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if (string.toLowerCase()===string) 
+  if (string.toLowerCase() === string) 
   return "I can't hear you!"
-  if (string.toUpperCase()===string) 
+  if (string.toUpperCase() === string) 
   return "YES INDEED!"
   if (string === "I love you, Grandma.")
   return "I love you, too."
